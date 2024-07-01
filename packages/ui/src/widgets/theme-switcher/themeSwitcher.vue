adbcb628b1b358070a3956c5e6a9788da41a21c4
Leonid Romanov <romanov.leonid@gmail.com>
2024-07-01 23:25:39 +0300

    fix(theme): created theme-switcher widget

---
+++ packages/ui/src/widgets/theme-switcher/themeSwitcher.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { AVAILABLE_COLOR_SCHEMES, useColorScheme } from '../../composables/useColorScheme.ts';
+  import AcvButton from '@/components/button/button.vue';
+  import AcvDropdown from '@/components/dropdown/dropdown.vue';
+
+  const { setColorScheme } = useColorScheme();
+</script>
+
+<template>
+  <AcvDropdown
+    trigger="click"
+    persist
+  >
+    <slot>
+      <AcvButton>
+        Change color scheme
+      </AcvButton>
+    </slot>
+    <template #dropdown>
+      <ul>
+        <li
+          v-for="(colorScheme, key) in AVAILABLE_COLOR_SCHEMES"
+          :key="key"
+          :data-test="`color-scheme-item-${key}`"
+          @click="setColorScheme(colorScheme)"
+        >
+          {{ colorScheme }}
+        </li>
+      </ul>
+    </template>
+  </AcvDropdown>
+</template>
